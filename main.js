c58ba736327adbbd3097b2827af24884b8c0720a
tony <tonetheman@gmail.com>
2023-08-19 13:56:30 -0500

    switched to modules slowly adding things

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
 
+// standard import for browser
+import kontra from "./node_modules/kontra/kontra.min.mjs";
 
 const STATE_HANG = 1; // hanging out at a base
 const STATE_MOVE = 2; // moving to a new base
 const STATE_DEAD = 3; // marked as dead
+const W = 600;
+const H = 400;
 
 let init = null;
 let canvas = null, context = null;
 let s = null;
+let blue_base = null;
+let red_base = null;
 
 // state - hanging out at a base
 // moving - moving from one base to another
@@ -31,18 +37,49 @@ class Fighter {
 // click on a base and send ALL available fighters
 // fighters shoudl cluster around a base
 class Base {
-    constructor(x,y,color,rate) {
+    constructor(x,y,color,rate,count) {
         this.x = x;
         this.y = y;
         this.color = color;
         this.rate = rate;
-    }
-    update() {
+        this.acc = 0;
+        this.count = count ? count : 0;
 
+        this.spr = kontra.Sprite({
+            x:x,y:y,color:color,width:32,height:32
+        });
+        this.txt = kontra.Text({
+            text: "0",
+            x:x+16,y:y+16,anchor:{x:0.5,y:0.5},
+            textAlign: 'center',
+            color:'white'
+        })
+    }
+    sprite() { return this.spr; }
+    update(dt) {
+        this.acc += dt;
+        if (this.acc>this.rate) {
+            this.count++;
+            this.acc = 0;
+            this.txt.text = this.count;
+        }
+        if (kontra.pointerOver(this.spr)) {
+            // highlight base?
+        }
+        this.spr.update();
+        this.txt.update();
     }
     render() {
-
+        this.spr.render();
+        this.txt.render();
     }
+}
+
+function _init() {
+    // setup globals
+    s = new Fighter(100,100,'blue');
+    blue_base = new Base(0,0,'blue',1);
+    red_base = new Base(W-32,H-32,'red',1);
 }
 
 function main() {
@@ -50,13 +87,23 @@ function main() {
     ({init} = kontra);
     ({canvas,context} = init())
 
-    s = new Fighter(100,100,'blue');
+    // set up global objects
+    _init();
+
+    kontra.initPointer();
+    kontra.track(blue_base.sprite());
+    kontra.track(red_base.sprite());
+
     let loop = kontra.GameLoop({
-        update: function() {
+        update: function(dt) {
             s.update();
+            blue_base.update(dt);
+            red_base.update(dt);
         },
         render: function() {
             s.render();
+            blue_base.render();
+            red_base.render();
         }
     });
     loop.start();
