f170a9fb24ac126473eda01c5661ee966196720b
tony <tonetheman@gmail.com>
2023-08-19 15:36:56 -0500

    trying to add attack groups not smart enough to understand tweens

--- main.js
+++ main.js
@@ -7,7 +7,11 @@ const STATE_MOVE = 2; // moving to a new base
 const STATE_DEAD = 3; // marked as dead
 const W = 600;
 const H = 400;
+
+// nothing happening
 const G_STATE_IDLE = 0;
+
+// player selected a base
 const G_STATE_BASE_SELECTED = 1;
 
 let init = null;
@@ -15,21 +19,45 @@ let canvas = null, context = null;
 let s = null;
 let good_bases = [];
 let bad_bases = [];
-let g_state = G_STATE_IDLE;
-let g_state_base = null; // currently selected base
+let groups = []; // attack groups
+let g = { 
+    state : G_STATE_IDLE,
+    base : null
+}
 
-// state - hanging out at a base
-// moving - moving from one base to another
-// dead -
-class Fighter {
-    constructor(x,y,color) {
-        this.spr = kontra.Sprite({
-            x:x,y:y,width:8,height:8,color:color
-        })
-        this.health = 10; // ?
-        this.state = STATE_HANG;
+class AttackGroup {
+    constructor(base,target,count) {
+        this.base = base;
+        this.target = target;
+        this.count = count;
+        this.spr = new kontra.Sprite({
+            x:this.base.spr.x,
+            y:this.base.spr.y,
+            image:kontra.imageAssets["ag"]
+        });
     }
-    update() {
+    // easing
+    // how much Time, Begin point, Change = end-beging,
+    // Duration - how much time has passed now?
+    linear(t,b,c,d) {
+        return c * t / d + b
+    }
+    update(dt) {
+        // move towards target?
+        
+        if (this.target.spr.x>this.base.spr.x) {
+            this.spr.x = this.spr.x + 1;
+        } else if (this.target.spr.x===this.base.spr.x) {
+        } else {
+            this.spr.x = this.spr.x - 1;
+        }
+        if (this.target.spr.y>this.base.spr.y) {
+            this.spr.y = this.spr.y + 1;
+        } else if (this.target.spr.y===this.base.spr.y){
+
+        } else  {
+            this.spr.y = this.spr.y - 1;
+        }
         this.spr.update();
     }
     render() {
@@ -69,6 +97,14 @@ class Base {
         })
     }
     sprite() { return this.spr; }
+    unselect() { 
+        if (this.color==='red') this.spr.image = kontra.imageAssets["rb_32x32"];
+        if (this.color==='blue') this.spr.image = kontra.imageAssets["bb_32x32"];
+    }
+    select() {
+        if (this.color==='red') this.spr.image = kontra.imageAssets["rb_selected_32x32"];
+        if (this.color==='blue') this.spr.image = kontra.imageAssets["bb_selected_32x32"];
+    }
     update(dt) {
         
         // growth handling
@@ -87,12 +123,32 @@ class Base {
         this.txt.render();
     }
     handle_onDown() {
-        this.selected = true;
-        this.spr.image = this.color==='red' ?
-        kontra.imageAssets["rb_selected_32x32"] :
-        kontra.imageAssets["bb_selected_32x32"];
-        g_state = G_STATE_BASE_SELECTED;
-        g_state_base = this;
+        if (this.color=='blue') {
+            if (g.state==G_STATE_BASE_SELECTED) {
+                g.state = G_STATE_IDLE;
+                g.base = null;
+                this.selected = false;
+                this.unselect();
+            } else {
+                g.state = G_STATE_BASE_SELECTED;
+                g.base = this;    
+                this.select();
+                this.selected = true;
+            }
+        } else if (this.color=='red') {
+            if (g.state===G_STATE_BASE_SELECTED) {
+                // this is a base we want to attack!
+                // 1. create an attack group
+                let tmp = new AttackGroup(g.base,this,g.base.count);
+                g.base.count = 0;
+                groups.push(tmp);
+
+                console.log("made a group!",tmp.count);
+                g.base.unselect();
+                g.state = G_STATE_IDLE;
+                g.base = null;
+            }
+        }
     }
     handle_onOver() {
 
@@ -107,13 +163,45 @@ function _init() {
     bad_bases.push(new Base(W-164,H-164,'red',1));
 }
 
+function global_onDown() {
+
+    // make sure we are not already
+    // handling this event in the sprite
+    for(let i=0;i<bad_bases.length;i++) {
+        if (kontra.pointerOver(bad_bases[i].sprite())) {
+            return;
+        }
+    }
+    for(let i=0;i<good_bases.length;i++) {
+        if (kontra.pointerOver(good_bases[i].sprite())) {
+            return;
+        }
+    }
+
+    // back to idle
+    g.state = G_STATE_IDLE;
+    // fix the image
+    g.base.spr.image = g.base.color==='red' ?
+    kontra.imageAssets["rb_32x32"] :
+    kontra.imageAssets["bb_32x32"];
+    // forget the selected base
+    g.base = null;
+}
+
 function _setup_tracking() {
+
+    // let each sprite handle its own tracking
     for (let i=0;i<good_bases.length;i++) {
         kontra.track(good_bases[i].sprite());    
     }
     for (let i=0;i<bad_bases.length;i++) {
         kontra.track(bad_bases[i].sprite());
     }
+
+    // setup one handler for global events
+    kontra.onPointer("down",()=>{
+        global_onDown();
+    })
 }
 
 function _main() {
@@ -136,6 +224,9 @@ function _main() {
             for(let i=0;i<bad_bases.length;i++) {
                 bad_bases[i].update(dt);
             }
+            for(let i=0;i<groups.length;i++) {
+                groups[i].update(dt);
+            }
         },
         render: function() {
             for(let i=0;i<good_bases.length;i++) {
@@ -143,6 +234,9 @@ function _main() {
             }
             for(let i=0;i<bad_bases.length;i++) {
                 bad_bases[i].render();
+            }
+            for(let i=0;i<groups.length;i++) {
+                groups[i].render();
             }
         }
     });
@@ -155,7 +249,8 @@ function main() {
         "rb_selected_32x32.png",
         "bb_32x32.png",
         "bb_selected_32x32.png",
-        "windmill.png")
+        "windmill.png",
+        "ag.png")
     .then(function(assets) {
         // g_assets = assets;
         //console.log("g_assets",g_assets)
